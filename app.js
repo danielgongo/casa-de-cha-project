31334fb7dfd949639b42ea3a6291924795a054fe
Daniel Gongo <dan_liveira@outlook.com>
2022-06-17 20:51:41 -0300

    iniciando os trabalhos

--- app.js
+++ app.js
@@ -1,7 +1,3 @@
-//setup date
-const date = (document.getElementById(
-  "date"
-).innerHTML = new Date().getFullYear());
 // setup nav
 const navBtn = document.getElementById("nav-btn");
 const navbar = document.getElementById("navbar");
@@ -14,3 +10,6 @@ navBtn.addEventListener("click", () => {
 navClose.addEventListener("click", () => {
   navbar.classList.remove("showNav");
 });
+//setup date
+const date = (document.getElementById("date").innerHTML =
+  new Date().getFullYear());
